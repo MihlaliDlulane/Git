66d0a9ff993c2d0a3b1bd720718e9a09c8549657
mihlali dlulane <midlulajhb024@student.wethinkcode.co.za>
2025-06-30 14:23:46 +0200

    finished stage 1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 
+some commands for me : 
+mkdir ~/Projects/Python/Git/tmp
+~/Projects/Python/Git/run.sh init
+echo "some text yeah" > text.txt
+git hash-object -w text.txt
+~/Projects/Python/Git/run.sh cat-file -p
--- app/main.py
+++ app/main.py
@@ -1,25 +1,66 @@
 import sys
 import os
 import zlib
+import argparse
+
+def initialize() -> None:
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+            f.write("ref: refs/heads/main\n")
+    print("Initialized git directory")
+
+def readGitObject(filename):
+    with open(f".git/objects/{filename[:2]}/{filename[2:]}", "rb") as f:
+        obj = zlib.decompress(f.read()).split(b"\x00")
+        return obj
+
+def readObjectContents(header) -> None:
+    contents = header[1]
+    print(contents.decode("utf-8"),end="")
+
+def readObjectType(header) -> None:
+    typ = header[0].split()[0]
+    print(typ.decode("utf-8"),end="")
+
+def readObjectSize(header) -> None:
+    ObjSize = header[0].split()[1]
+    print(ObjSize.decode("utf-8"),end="")
+           
 
 def main():
 
-    command = sys.argv[1]
-    if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
-    elif command == "cat-file" and sys.argv[2] == "-p":
-        filename = sys.argv[3]
-        with open(f".git/objects/{filename[:2]}/{filename[2:]}", "rb") as f:
-            blob = zlib.decompress(f.read()).split(b"\x00")[1]
-            print(blob.decode("utf-8"), end="")
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='main_command',required=True)
+
+    #init parser
+    init_parser = subparser.add_parser('init')
+
+    #cat file parser
+    cat_file_parser = subparser.add_parser('cat-file')
+
+    operation_group = cat_file_parser.add_mutually_exclusive_group(required= True)
+    operation_group.add_argument('-t','--type',action='store_true')
+    operation_group.add_argument('-p','--print',action='store_true')
+    operation_group.add_argument('-s','--size',action='store_true')
+    cat_file_parser.add_argument('filename')
+
+    args = parser.parse_args()
+
+    if args.main_command == "init":
+        initialize()
+    elif args.main_command == "cat-file":
+        obj = readGitObject(args.filename)
+        if args.type:
+            readObjectType(obj)
+        elif args.print:
+            readObjectContents(obj)
+        elif args.size:
+            readObjectSize(obj)    
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
 
 if __name__ == "__main__":
-    main()
+   main()
